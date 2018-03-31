5fa8afd0a2b6cae29ea05f6c39f04367eb856239
wzr1337 <bartsch98@gmail.com>
2018-03-31 14:59:08 +0200

    refac(general) use rsi.core 0.2.4

--- dist/renderers.d.ts
+++ dist/renderers.d.ts
@@ -1,15 +1,10 @@
-import { BehaviorSubject } from "rxjs";
-import { CollectionResponse, ElementResponse, IElement, Resource, Service } from "@rsi/core";
+import { ElementResponse, Resource, Service } from "@rsi/core";
 import { ICollectionObject } from "./media.types";
 export declare class Renderers extends Resource {
     private mediaCollection;
-    private renderers;
-    private nR;
+    private netfluxRenderer;
     private logger;
     constructor(service: Service, mediaCollection: ICollectionObject);
     readonly elementSubscribable: boolean;
-    readonly elements: Array<BehaviorSubject<IElement>>;
-    getElement(elementId: string): Promise<ElementResponse>;
-    getResource(offset?: string | number, limit?: string | number): Promise<CollectionResponse>;
     updateElement(elementId: string, difference: any): Promise<ElementResponse>;
 }
--- dist/tracks.d.ts
+++ dist/tracks.d.ts
@@ -1,10 +1,5 @@
-import { CollectionResponse, ElementResponse, IElement, Resource, Service } from "@rsi/core";
-import { BehaviorSubject } from "rxjs";
+import { Resource, Service } from "@rsi/core";
 export declare class Tracks extends Resource {
-    private tracks;
     constructor(service: Service);
     readonly elementSubscribable: boolean;
-    readonly elements: Array<BehaviorSubject<IElement>>;
-    getElement(elementId: string): Promise<ElementResponse>;
-    getResource(offset?: string | number, limit?: string | number): Promise<CollectionResponse>;
 }
