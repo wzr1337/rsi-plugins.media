e6ee5d81f9e908c4837a24a53904f702f200c8f0
wzr1337 <bartsch98@gmail.com>
2018-04-26 22:14:16 +0200

    chore(logging):  disable cdn logging for tests

--- dist/media/index.spec.js
+++ dist/media/index.spec.js
@@ -1,6 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@rsi/core");
 var _1 = require(".");
+beforeAll(function () {
+    console.log(core_1.RsiLogger.getInstance().getLogger("cdn").level = "error");
+});
 describe("Media", function () {
     it("should have an id", function () {
         var media = _1.Media.getInstance();
--- src/media/index.spec.ts
+++ src/media/index.spec.ts
@@ -1,4 +1,9 @@
+import { RsiLogger } from "@rsi/core";
 import { Media } from ".";
+
+beforeAll(() => {
+  console.log(RsiLogger.getInstance().getLogger("cdn").level = "error"); // disbale cdn silly logs
+});
 
 describe("Media", () => { // empty
   it ("should have an id", () => {
